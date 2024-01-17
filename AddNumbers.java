55d91055618ff4eb8b67822bb68e7ec21c1894ae
shaik-barkath <shaikbarkath44@gmail.com>
2024-01-17 20:39:56 +0530

    Program 1

---
+++ AddNumbers.java
@@ -0,0 +1,13 @@
+
+public class AddNumbers {
+
+	public static void main(String[] args) {
+    int a=10,b=20; //local variable
+    addnumbers(a,b);
+	}
+	public static void addnumbers(int x,int y) {
+		int sum = x+y;
+		System.out.println("The sum ="+sum);
+	}
+
+}
